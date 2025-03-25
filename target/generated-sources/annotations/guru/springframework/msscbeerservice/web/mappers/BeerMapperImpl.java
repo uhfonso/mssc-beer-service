64ffdafeeb143e334e8c8d23f3535c46dca48362
afonso.sampaio@tectius.com <afonsodev@outlook.com>
2025-03-25 17:16:47 +0000

    first commit

---
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-24T17:48:16+0000",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
